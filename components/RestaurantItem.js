edbffbd64a690060a954bfcd72c7dab34bda80ac
mohammad <mohammadnv79@gmail.com>
2023-08-09 16:24:01 +0330

    add rating

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,18 +1,21 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+
+import StarRating from "react-native-star-rating-widget";
 
 export default function RestaurantItem(props) {
   const { imageSrc, title, rate, estimateTime } = props;
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <Image source={imageSrc} style={styles.imageHolders} />
       <View style={styles.foodInfo}>
         <View>
           <Text style={styles.foodTitle}>{title}</Text>
           <Text style={styles.foodEstimate}>{estimateTime} min</Text>
         </View>
+        <StarRating rating={rate} enableSwiping={false} onChange={() => {}} starSize={20} starStyle={{margin: 0, padding: 0}} />
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
@@ -29,6 +32,9 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   foodInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 10,
   },
   foodTitle: {
